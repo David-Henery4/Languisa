f06f4e6ae85d5c32bf0c1c4ff91720952a4b577b
DizzlyD <david4henery00@hotmail.com>
2022-06-10 00:22:24 +0100

    greeting-Animation

--- J.S/main.js
+++ J.S/main.js
@@ -1,4 +1,4 @@
-console.log("working")
+console.log("working");
 
 // Burger Element
 const burger = document.querySelector(".burger");
@@ -15,34 +15,34 @@ const burgerLine3 = document.querySelector(".burger__line-3");
 
 // Burger & Sidenav Functionality
 const burgerAnimation = () => {
-    burgerLine1.classList.toggle("burger-line-1");
-    burgerLine2.classList.toggle("burger-line-2");
-    burgerLine3.classList.toggle("burger-line-3");
-}
+  burgerLine1.classList.toggle("burger-line-1");
+  burgerLine2.classList.toggle("burger-line-2");
+  burgerLine3.classList.toggle("burger-line-3");
+};
 const toggleSideNav = () => {
-    sideNavBackground.classList.toggle("side-nav--active");
-    sideNav.classList.toggle("side-nav-wrap--active");
-}
+  sideNavBackground.classList.toggle("side-nav--active");
+  sideNav.classList.toggle("side-nav-wrap--active");
+};
 
 // Burger & Sidenav Events
 burger.addEventListener("click", () => {
-    toggleSideNav()
-    burgerAnimation()
-})
+  toggleSideNav();
+  burgerAnimation();
+});
 sideNavCloseIcon.addEventListener("click", () => {
-    toggleSideNav();
-    burgerAnimation()
-})
+  toggleSideNav();
+  burgerAnimation();
+});
 //*********************************//
 
 const sidenavItems = document.querySelector(".side-nav__list");
 
 sidenavItems.addEventListener("click", (e) => {
-    const clicked = e.target.closest(".side-nav__link");
-    if (!clicked) return
-    burgerAnimation()
-    toggleSideNav()
-})
+  const clicked = e.target.closest(".side-nav__link");
+  if (!clicked) return;
+  burgerAnimation();
+  toggleSideNav();
+});
 
 //*****************************************************//
 
@@ -52,103 +52,136 @@ const rightArrow = document.querySelector(".testimonial__slide-right-arrow");
 const dotsContainer = document.querySelector(".testimonial__slider-dots");
 let curSlide = 0;
 const maxSlide = slides.length;
-console.log(maxSlide)
-
+// console.log(maxSlide)
 
 const goToSlide = (slide) => {
-    slides.forEach((s,i) => s.style.transform = `translateX(${125*(i - slide)}%)`)
-    console.log(slide)
-    // activeDot(slide)
-}
+  slides.forEach(
+    (s, i) => (s.style.transform = `translateX(${125 * (i - slide)}%)`)
+  );
+  // console.log(slide)
+  // activeDot(slide)
+};
 
 const nextSlide = () => {
-    if (curSlide === maxSlide -1) {
-        curSlide = 0;
-    } else{
-        curSlide++
-    }
-    goToSlide(curSlide)
-    activeDot(curSlide)
-}
+  if (curSlide === maxSlide - 1) {
+    curSlide = 0;
+  } else {
+    curSlide++;
+  }
+  goToSlide(curSlide);
+  activeDot(curSlide);
+};
 
 const prevSlide = () => {
-    if (curSlide === 0){
-        curSlide = maxSlide -1;
-    } else {
-        curSlide--;
-    }
-    goToSlide(curSlide)
-    activeDot(curSlide)
-}
+  if (curSlide === 0) {
+    curSlide = maxSlide - 1;
+  } else {
+    curSlide--;
+  }
+  goToSlide(curSlide);
+  activeDot(curSlide);
+};
 
 const creatingDots = () => {
-    slides.forEach((_,i) => {
-        dotsContainer.insertAdjacentHTML(
-          "beforeend",
-          `<div class="dot" data-slide=${i}></div>`
-        );
-    })
-}
+  slides.forEach((_, i) => {
+    dotsContainer.insertAdjacentHTML(
+      "beforeend",
+      `<div class="dot" data-slide=${i}></div>`
+    );
+  });
+};
 
 const activeDot = (activeSlide) => {
-    const dots = document.querySelectorAll(".dot")
-    const currentDot = document.querySelector(`.dot[data-slide="${activeSlide}"]`)
-    dots.forEach(e => e.classList.remove("dot-active"))
-    currentDot.classList.add("dot-active")
-}
+  const dots = document.querySelectorAll(".dot");
+  const currentDot = document.querySelector(
+    `.dot[data-slide="${activeSlide}"]`
+  );
+  dots.forEach((e) => e.classList.remove("dot-active"));
+  currentDot.classList.add("dot-active");
+};
 
 dotsContainer.addEventListener("click", (e) => {
-    if (e.target.closest(".dot")){
-        const slide = e.target.dataset.slide
-        goToSlide(+slide)
-        activeDot(+slide)
-    }
-})
+  if (e.target.closest(".dot")) {
+    const slide = e.target.dataset.slide;
+    goToSlide(+slide);
+    activeDot(+slide);
+  }
+});
 
 document.addEventListener("keydown", (e) => {
-    if (e.key === "ArrowLeft") prevSlide()
-    e.key === "ArrowRight" && nextSlide()
-})
+  if (e.key === "ArrowLeft") prevSlide();
+  e.key === "ArrowRight" && nextSlide();
+});
 
 rightArrow.addEventListener("click", () => {
-    nextSlide()
-})
+  nextSlide();
+});
 
 leftArrow.addEventListener("click", () => {
-    prevSlide()
-})
+  prevSlide();
+});
 
 const init = () => {
-    goToSlide(0)
-    creatingDots()
-    activeDot(0)
+  goToSlide(0);
+  creatingDots();
+  activeDot(0);
+};
+
+init();
+
+//*********************************************//
+
+//****************************************************//
+// HEADER ANIMATION #4
+
+const greetingContainer = document.querySelector(".header-greeting-container");
+const greetingSlider = document.querySelector(".header__title-box");
+let greetingSlides = document.querySelectorAll(".header__title-wrap");
+const firstClone = greetingSlides[0].cloneNode(true)
+const secondClone = greetingSlides[greetingSlides.length - 1].cloneNode(true)
+//
+greetingSlider.append(firstClone)
+greetingSlider.prepend(secondClone)
+firstClone.id = "first-clone"
+secondClone.id = "second-clone"
+//
+const interval = 5000
+let counter = 1
+
+
+
+const setClones = () => {
+  greetingSlides = document.querySelectorAll(".header__title-wrap");
+    greetingSlides.forEach((greeting, i) => {
+    greeting.style.transform = `translateX(${125 * (i - counter)}%)`;
+  });
+}
+setClones()
+
+const startAni = () => {
+  setInterval(() => {
+    counter++
+    greetingSlides.forEach((greeting, i) => {
+      greeting.style.transform = `translateX(${125 * (i - counter)}%)`;
+      greeting.style.transition = ".7s";
+    });
+  }, interval)
 }
 
-init()
+greetingSlider.addEventListener("transitionend", () => {
+  greetingSlides = document.querySelectorAll(".header__title-wrap");
+  if (greetingSlides[counter].id === firstClone.id){
+    // greetingSlider.style.transition = "none";
+    counter = 1;
+      greetingSlides.forEach((greeting, i) => {
+        greeting.style.transition = "none";
+        greeting.style.transform = `translateX(${125 * (i - counter)}%)`;
+      });
+  }
+})
 
-// HEADER ANIMATION
+startAni()
 
-const hellos = document.querySelectorAll(".header__title-wrap")
-let currentGreeting = 0
-const maxGreatings = hellos.length
+//*******************************************************//
 
-const headerAni = (current) => {
-    console.log(hellos)
-    hellos.forEach((greeting, i) => 
-        greeting.style.transform = `translateX(${125 * (i - current)}%)`
-    )
-}
 
-// headerAni()
-
-const nextGreeting = () => {
-    if (currentGreeting > maxGreatings - 1){
-        currentGreeting = 0
-    } else {
-        currentGreeting++
-    }
-    headerAni(currentGreeting)
-}
-
-nextGreeting()
-// console.log(currentGreeting)
