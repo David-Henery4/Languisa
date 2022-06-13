16baab4023b4294fe323a9386a3a374a9e41ff5f
DizzlyD <david4henery00@hotmail.com>
2022-06-13 23:11:48 +0100

    sidenav-refactor

--- J.S/main.js
+++ J.S/main.js
@@ -3,15 +3,16 @@ console.log("working");
 // Burger Element
 const burger = document.querySelector(".burger");
 
-// Side-Nave
-const sideNavBackground = document.querySelector(".side-nav");
-const sideNav = document.querySelector(".side-nav-wrap");
-const sideNavCloseIcon = document.querySelector(".side-nav-close");
-
 // Burger animation
 const burgerLine1 = document.querySelector(".burger__line-1");
 const burgerLine2 = document.querySelector(".burger__line-2");
 const burgerLine3 = document.querySelector(".burger__line-3");
+
+// Side-Nav
+const sideNavBackground = document.querySelector(".side-nav-background");
+const sideNav = document.querySelector(".side-nav");
+const sideNavCloseIcon = document.querySelector(".side-nav-close");
+
 
 // Burger & Sidenav Functionality
 const burgerAnimation = () => {
@@ -20,7 +21,7 @@ const burgerAnimation = () => {
   burgerLine3.classList.toggle("burger-line-3");
 };
 const toggleSideNav = () => {
-  sideNavBackground.classList.toggle("side-nav--active");
+  sideNavBackground.classList.toggle("side-nav-background--active");
   sideNav.classList.toggle("side-nav-wrap--active");
 };
 
@@ -33,6 +34,10 @@ sideNavCloseIcon.addEventListener("click", () => {
   toggleSideNav();
   burgerAnimation();
 });
+sideNavBackground.addEventListener("click", (e) => {
+  toggleSideNav();
+  burgerAnimation();
+})
 //*********************************//
 
 const sidenavItems = document.querySelector(".side-nav__list");
